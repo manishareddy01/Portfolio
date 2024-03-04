b36895ead4cf278eecc814ab08aa22556e097cdf
unknown <manishaaddula@gmail.com>
2024-03-04 15:08:18 -0600

    portfolio added-removed apos

--- components/Works/AIESEC.tsx
+++ components/Works/AIESEC.tsx
@@ -10,7 +10,7 @@ const AIESEC = () => {
         <p className='text-sm mt-1 font-medium text-textDark'>Aug-2022 to Dec-2022</p>
         <ul className='mt-6 flex flex-col gap-3'>
             <li className='text-base text-textDark gap-2 flex'>
-                <span className='text-textGreen mt-1'><TiArrowForward/></span>Managed the company's social media accounts, creating and scheduling posts and engaging with followers, resulting in a 20% increase in social media engagement.
+                <span className='text-textGreen mt-1'><TiArrowForward/></span>Managed the social media accounts, creating and scheduling posts and engaging with followers, resulting in a 20% increase in social media engagement.
             </li>
             <li className='text-base text-textDark gap-2 flex'>
                 <span className='text-textGreen mt-1'><TiArrowForward/></span>Developed and executed marketing campaigns across various channels such as email, social media, and digital platforms, resulting in a 25% increase in lead generation.
@@ -19,10 +19,10 @@ const AIESEC = () => {
                 <span className='text-textGreen mt-1'><TiArrowForward/></span>Assisted in event planning, resulting in a successful company event that attracted over 500 attendees.
             </li>
             <li className='text-base text-textDark gap-2 flex'>
-                <span className='text-textGreen mt-1'><TiArrowForward/></span>Won 'BEST TEAM AWARD' in NLDS 2021.
+                <span className='text-textGreen mt-1'><TiArrowForward/></span>Won BEST TEAM AWARD in NLDS 2021.
             </li>
             <li className='text-base text-textDark gap-2 flex'>
-                <span className='text-textGreen mt-1'><TiArrowForward/></span>Won 'BEST PROGRESSIVE TEAM AWARD'.
+                <span className='text-textGreen mt-1'><TiArrowForward/></span>Won BEST PROGRESSIVE TEAM AWARD.
             </li>
         </ul>
     </motion.div>
